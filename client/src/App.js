d4728418e8405b237297606e91837705a681d190
Danny Tran <danny.qa.tran@gmail.com>
2019-12-27 00:35:49 -0800

    Add landing page and logo

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from "./components/auth/Login"
 import SignUp from "./components/auth/SignUp"
+import Landing from "./components/layout/Landing";
 
 
 function App() {
   return (
     <Router>
       <div>
-        <Route path="/" exact component={Login} />
+        <Route path="/" exact component={Landing} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={SignUp} />
       </div>
