b5d121476d62145e87a9e93af98ca6aa2dc25455
Danny Tran <danny.qa.tran@gmail.com>
2020-01-06 19:30:30 -0800

    Set up redux following this tutorial: https://blog.bitsrc.io/build-a-login-auth-app-with-mern-stack-part-2-frontend-6eac4e38ee82

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Login from "./components/auth/Login"
 import SignUp from "./components/auth/SignUp"
 import Landing from "./components/layout/Landing";
@@ -7,13 +11,15 @@ import Landing from "./components/layout/Landing";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route path="/" exact component={Landing} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={SignUp} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Route path="/" exact component={Landing} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
